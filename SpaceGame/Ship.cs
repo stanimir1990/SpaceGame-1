1e05e795db4b65628f70c5926f2e0ecd2580f3e5
Teodor Tunev <teodortunev@gmail.com>
2014-02-09 12:27:15 +0200

    Ship

---
+++ SpaceGame/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    public abstract class Ship
+    {
+        private int healthPoint;
+        private int attackDamage;
+
+        public Ship(int healthPoint , int attackDamage)
+        {
+            this.AttackDamage = attackDamage;
+            this.HealthPoint = healthPoint;
+        }
+
+        public int AttackDamage
+        {
+            get { return attackDamage; }
+            set { attackDamage = value; }
+        }
+        
+
+        public int HealthPoint
+        {
+            get { return healthPoint; }
+            set { healthPoint = value; }
+        }
+        
+    }
+}
